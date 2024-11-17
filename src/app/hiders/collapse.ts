df3701018c786d8826b9a8a495a4b15eb0b9b61e
Pranav Gade <pranav@conjecture.dev>
2024-11-17 10:37:02 +0000

    final touches

--- src/app/hiders/collapse.ts
+++ src/app/hiders/collapse.ts
@@ -57,7 +57,7 @@ function createClipCollapse(
     justifyContent: 'center',
     zIndex: zIndex.toString()
   });
-  clipButton.innerHTML = '<svg style="stroke: #4299e1; stroke-width: 2; stroke-linecap: round; stroke-linejoin: round; fill: none; filter: drop-shadow(0 0 2px white)" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M21.44 11.05l-9.19 9.19a6 6 0 0 1-8.49-8.49l9.19-9.19a4 4 0 0 1 5.66 5.66l-9.2 9.19a2 2 0 0 1-2.83-2.83l8.49-8.48"/></svg>';
+  clipButton.innerHTML = '<svg style="stroke: #4299e1; stroke-width: 2; stroke-linecap: round; stroke-linejoin: round; fill: none; filter: drop-shadow(0 0 1px white)" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M21.44 11.05l-9.19 9.19a6 6 0 0 1-8.49-8.49l9.19-9.19a4 4 0 0 1 5.66 5.66l-9.2 9.19a2 2 0 0 1-2.83-2.83l8.49-8.48"/></svg>';
 
   // Add clip button to document body
   document.body.appendChild(clipButton);
